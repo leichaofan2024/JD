3566414a7d5249e167df0b66a04d3e0337844838
leichaofan <leichaofan2024@gmail.com>
2017-03-03 22:26:58 +0800

    add a new page contact

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "products#index"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get "/contact", to: "welcome#contact"
   namespace :admin do
     resources :products
     resources :orders do
